778644500a59ad66c061dfda92a34f7f70dd34e7
Romario <23100325@ue.edu.pe>
2026-05-28 18:26:39 -0500

    a

---
+++ PC12410003323100833.WEBAPI/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace PC12410003323100833.WEBAPI
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
