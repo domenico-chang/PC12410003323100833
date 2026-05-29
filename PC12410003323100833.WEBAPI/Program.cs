60a4fc149858fa1602353bf12a78654b413de67f
domenico-chang <24100033@ue.edu.pe>
2026-05-28 19:03:48 -0500

    settings

--- PC12410003323100833.WEBAPI/Program.cs
+++ PC12410003323100833.WEBAPI/Program.cs
@@ -1,21 +1,25 @@
+using Microsoft.EntityFrameworkCore;
 using PC12410003323100833.CORE.Core.Interfaces;
 using PC12410003323100833.CORE.Core.Services;
+using PC12410003323100833.CORE.Infrastructure.Data;
 using PC12410003323100833.CORE.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var _config = builder.Configuration;
+var cnx = _config.GetConnectionString("DevConnection");
+
+builder.Services.AddDbContext<TallerMecanicoContext>(options =>
+    options.UseSqlServer(cnx));
+
+builder.Services.AddTransient<IOrdenServicioRepository, OrdenServicioRepository>();
+builder.Services.AddTransient<IOrdenServicioService, OrdenServicioService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
-builder.Services.AddScoped<IOrdenServicioService, OrdenServicioService>();
-builder.Services.AddScoped<IOrdenServicioRepository, OrdenServicioRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
